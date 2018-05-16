6b22a802bbdfd0c1c4d2bde194a7c4d9d75f8705
Noctis <Noctis@192.168.0.16>
2018-05-15 22:40:08 -0400

    test

---
+++ src/main/java/com/dixon/project/Project.java
@@ -0,0 +1,124 @@
+package com.dixon.project;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+public class Project {
+	
+	/*private static final Logger logger = LogManager.getLogger();
+	//com.dixon.project.Project$Person@67784306
+	*/
+
+	
+	/*
+	//Example 1 Start
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		Person anthony = new Person();		
+		anthony.name = "Anthony";		
+		anthony.age = 24;
+		anthony.f = 23;
+		
+		Person miranda = new Person();
+		miranda.name = "Miranda";
+		
+		System.out.println(anthony.name);
+		System.out.println(anthony.age);
+		System.out.println(miranda.name);
+		System.out.println(miranda.age);
+		System.out.println(anthony.f);
+	
+	}
+	
+	
+	public static class Person {
+		
+		int age;
+		String name;
+		float f;
+		
+	}
+		
+}
+
+//Example 1 End
+*/
+
+	/*	
+	//Question One Start
+	
+	public static void main(String[] args) {
+			
+		
+		int start = 0;
+		int limit = 100;
+		
+		while (start < limit){
+			
+			start = start + 1;
+			System.out.println(start);
+			
+		}
+		
+	}
+	
+}
+		
+//Question One End
+ */
+
+	
+/*	
+//Question Three Start
+
+	public static void main(String[] args) {
+				
+		int number = 4;
+		int squared = 0;
+		int multiplier = 2;
+		
+		
+		squared = (int) Math.pow(number, multiplier);	
+		
+		System.out.println(squared);
+		
+	}
+}
+		
+//Question Three End
+*/
+	
+/*		
+//Question Two Start
+
+	public static void main(String[] args) {
+		
+	
+	int value = 4;
+	int total = 0;
+	
+	total = displayScreen(value);
+	
+	System.out.println(total);
+	
+	}
+	
+	
+	public static int displayScreen(int value) {
+		
+		value = value * value;
+		return value;		
+	
+	
+	}
+	
+}
+
+//Question Two End
+*/
+
+
+	
